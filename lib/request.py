834ba6ecb03bb26b7e910183725b81210251ad61
damaainan <damaainan@163.com>
2020-06-24 11:42:56 +0800

    init

---
+++ lib/request.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+# -*- coding: utf-8
+
+'''
+请求方法类
+Created on 2020-06-24 11:27:58
+@author: jch
+'''
+
+
+# get 请求
+# post 请求
---
+++ run.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+# -*- coding: utf-8
+
+'''
+执行测试方法
+Created on 2020-06-24 11:41:00
+@author: jch
+'''
